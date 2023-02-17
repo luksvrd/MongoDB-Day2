584977e570466fd4b04a0a1030ae5db2ca0ba665
luksvrd <ljvy9b@gmail.com>
2023-02-16 19:53:52 -0600

    npm i mongoose & express

--- app/index.js
+++ app/index.js
@@ -1,1 +1,76 @@
-export const add2Nums = (num1, num2) => num1 + num2;
+import fs from "fs/promises";
+
+const data = [
+  {
+    name: "John Doe",
+    grades: [
+      {
+        gradeType: "exam",
+        name: "Exam 1",
+        earned: 85,
+        possible: 100,
+      },
+      {
+        gradeType: "homework",
+        name: "Assignment 1",
+        earned: 95,
+        possible: 100,
+      },
+      {
+        gradeType: "quiz",
+        name: "Quiz 1",
+        earned: 18,
+        possible: 20,
+      },
+    ],
+  },
+  {
+    name: "Jane Smith",
+    grades: [
+      {
+        gradeType: "exam",
+        name: "Exam 1",
+        earned: 92,
+        possible: 100,
+      },
+      {
+        gradeType: "homework",
+        name: "Assignment 1",
+        earned: 88,
+        possible: 100,
+      },
+      {
+        gradeType: "quiz",
+        name: "Quiz 1",
+        earned: 16,
+        possible: 20,
+      },
+    ],
+  },
+  {
+    name: "Bob Johnson",
+    grades: [
+      {
+        gradeType: "exam",
+        name: "Exam 1",
+        earned: 75,
+        possible: 100,
+      },
+      {
+        gradeType: "homework",
+        name: "Assignment 1",
+        earned: 80,
+        possible: 100,
+      },
+      {
+        gradeType: "quiz",
+        name: "Quiz 1",
+        earned: 18,
+        possible: 20,
+      },
+    ],
+  },
+];
+
+// TODO: 'JSON-ify the data array and write it to a file called "data.json"'
+fs.writeFile("data.json", JSON.stringify(data));
